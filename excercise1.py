d9a75f5614c17a519cdd015f66aaa6ae6b279f04
anand14327sagar <50095773+anand14327sagar@users.noreply.github.com>
2020-05-05 13:16:09 +0530

    Week1 :  Day1 Excercises

---
+++ excercise1.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello ATS Team")
+
+if __name__ == "__main__":
+    main()
---
+++ excercise3.py
@@ -0,0 +1,10 @@
+num1 = int(raw_input("Enter 1st number : "))
+print("Num1: "+str(num1)) 
+num2 = int(raw_input("Enter 1st number : ")) 
+print("Num1: "+ str(num2)) 
+
+sum = num1 + num2
+mul = num1 * num2
+
+print("Sum is: "+str(sum))
+print("Multiplication is: "+str(mul))
