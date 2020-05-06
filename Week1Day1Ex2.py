5104138ba233c1c414272da5cb8376df9278f939
anand14327sagar <50095773+anand14327sagar@users.noreply.github.com>
2020-05-06 11:15:40 +0530

    Week1:Day1

---
+++ Week1Day1Ex2.py
@@ -0,0 +1,24 @@
+var1 = "hello"
+var2 = 2020
+var3 = 1.0
+
+if var1 == 'hello':
+    print("String")
+elif var1 == 2020:
+    print("Integer") 
+elif var1 == 1.0:
+    print("Float")    
+
+if var2 == 'hello':
+    print("String")
+elif var2 == 2020:
+    print("Integer") 
+elif var2 == 1.0:
+    print("Float")  
+
+if var3 == 'hello':
+    print("String")
+elif var3 == 2020:
+    print("Integer") 
+elif var3 == 1.0:
+    print("Float")      
